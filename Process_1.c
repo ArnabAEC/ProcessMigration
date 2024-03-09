869df7269378c9c14899166f3221389bb4069d35
ArnabAEC <noonevs@gmail.com>
2024-03-09 16:38:29 +0530

    update for Windows

--- Process_1.c
+++ Process_1.c
@@ -2,21 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
 
 #define PORT 12345
 
 int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        perror("Failed to initialize Winsock");
+        return EXIT_FAILURE;
+    }
+
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size = sizeof(struct sockaddr_in);
+    int addr_size = sizeof(struct sockaddr_in);
 
     // Create socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    if (server_socket == INVALID_SOCKET) {
         perror("Error creating socket");
-        exit(EXIT_FAILURE);
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
     // Prepare the server address struct
@@ -25,44 +31,49 @@ int main() {
     server_addr.sin_port = htons(PORT);
 
     // Bind the socket
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         perror("Error binding socket");
-        close(server_socket);
-        exit(EXIT_FAILURE);
+        closesocket(server_socket);
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
     // Listen for incoming connections
-    if (listen(server_socket, 10) == -1) {
+    if (listen(server_socket, 10) == SOCKET_ERROR) {
         perror("Error listening for connections");
-        close(server_socket);
-        exit(EXIT_FAILURE);
+        closesocket(server_socket);
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
     printf("Peer A listening on port %d\n", PORT);
 
     // Accept a connection
     client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
-    if (client_socket == -1) {
+    if (client_socket == INVALID_SOCKET) {
         perror("Error accepting connection");
-        close(server_socket);
-        exit(EXIT_FAILURE);
+        closesocket(server_socket);
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
     // Receive data from Peer B
     char received_data[256];
-    ssize_t recv_size = recv(client_socket, received_data, sizeof(received_data), 0);
+    int recv_size = recv(client_socket, received_data, sizeof(received_data), 0);
     if (recv_size <= 0) {
         perror("Error receiving data");
-        close(server_socket);
-        close(client_socket);
-        exit(EXIT_FAILURE);
+        closesocket(server_socket);
+        closesocket(client_socket);
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
     printf("Received data from Peer B: %s\n", received_data);
 
     // Close sockets
-    close(server_socket);
-    close(client_socket);
+    closesocket(server_socket);
+    closesocket(client_socket);
+    WSACleanup();
 
     return 0;
 }
--- Process_2.c
+++ Process_2.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
 
 #define SERVER_IP "127.0.0.1"
 #define PORT 12345
 
 int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        perror("Failed to initialize Winsock");
+        return EXIT_FAILURE;
+    }
+
     int client_socket;
     struct sockaddr_in server_addr;
 
     // Create socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
+    if (client_socket == INVALID_SOCKET) {
         perror("Error creating socket");
+        WSACleanup();
         exit(EXIT_FAILURE);
     }
 
@@ -25,23 +31,26 @@ int main() {
     server_addr.sin_port = htons(PORT);
 
     // Connect to Peer A
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         perror("Error connecting to Peer A");
-        close(client_socket);
+        closesocket(client_socket);
+        WSACleanup();
         exit(EXIT_FAILURE);
     }
 
     // Send data to Peer A
     char data_to_send[] = "Hello from Peer B";
-    ssize_t send_size = send(client_socket, data_to_send, sizeof(data_to_send), 0);
-    if (send_size == -1) {
+    int send_size = send(client_socket, data_to_send, sizeof(data_to_send), 0);
+    if (send_size == SOCKET_ERROR) {
         perror("Error sending data");
-        close(client_socket);
+        closesocket(client_socket);
+        WSACleanup();
         exit(EXIT_FAILURE);
     }
 
     // Close socket
-    close(client_socket);
+    closesocket(client_socket);
+    WSACleanup();
 
     return 0;
 }
